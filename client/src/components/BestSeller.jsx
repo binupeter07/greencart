8a59f0bb2a33ef54cd21c25d944dfbd9c59d5c48
Binu Peter <binupeterp07@gmail.com>
2025-04-22 22:04:17 +0100

    Homepage frontend finished

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,7 +7,7 @@ const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-3 md:gap-6 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-4 md:gap-6 mt-6">
         {products
           .filter((product) => product.inStock)
           .slice(0, 5)
